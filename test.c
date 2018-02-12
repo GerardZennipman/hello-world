61aaeb9446117f34b2a26bc7509b08f1db20ab5c
GerardZennipman <36416902+GerardZennipman@users.noreply.github.com>
2018-02-12 18:24:50 +0100

    Add files via upload
    Added test.c and test.h to repository hello-world

---
+++ test.c
@@ -0,0 +1,11 @@
+#include "standard.h"
+#include "test.h"
+
+int32_t test_add(int32_t a, int32_t b)
+{
+    int32_t sum;
+
+    sum = a + b;
+
+    return sum;
+}
---
+++ test.h
@@ -0,0 +1,10 @@
+/*
+ * standard.h
+ */
+
+#ifndef TEST_H
+#define TEST_H
+
+int32_t test_add(int32_t a, int32_t b);
+
+#endif // TEST_H
