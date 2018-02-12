063b00777ac1661f0c56e893ab702c0f33b3d1a7
GerardZennipman <36416902+GerardZennipman@users.noreply.github.com>
2018-02-12 20:20:10 +0100

    Update test.c
    Add test_sub()

--- test.c
+++ test.c
@@ -9,3 +9,12 @@ int32_t test_add(int32_t a, int32_t b)
 
     return sum;
 }
+
+int32_t test_sub(int32_t a, int32_t b)
+{
+    int32_t difference;
+
+    difference = a - b;
+
+    return difference;
+}
