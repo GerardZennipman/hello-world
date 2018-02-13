1e319ecd3a8418b0fcf4fe900758bece6a3bbb8e
GerardZennipman <36416902+GerardZennipman@users.noreply.github.com>
2018-02-13 20:08:15 +0100

    Update test.h
    Add this line on internet to test.h

--- test.h
+++ test.h
@@ -12,5 +12,6 @@ int32_t test_div(int32_t a, int32_t b);
 
 // Add new functions below this line
 // Add this line on PC to local repository use single commit test.c test.h
+// Add this line on internet
 
 #endif // TEST_H
